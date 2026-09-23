35105dbf285ac1264b180f0cd9b83d750046b5c0
Dante <yujradanner92@gmail.com>
2026-09-22 20:47:25 -0400

    Implementa sesiones y proteccion de ruta

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+
+app.secret_key = "clave-secreta-portal-biblioteca"
 
 usuarios = {
     "carlos": "1111",
@@ -23,12 +25,39 @@ def login():
         contraseña = request.form["contraseña"]
 
         if usuario in usuarios and usuarios[usuario] == contraseña:
-            return "Inicio de sesión correcto"
+            session["usuario"] = usuario
+            return redirect(url_for("libros"))
 
         else:
             return "Usuario o contraseña incorrectos."
 
     return render_template("login.html")
+
+
+@app.route("/libros")
+def libros():
+
+    if "usuario" not in session:
+        return redirect(url_for("login"))
+
+    return "Bienvenido a la biblioteca"
+
+
+@app.route("/perfil")
+def perfil():
+
+    if "usuario" not in session:
+        return redirect(url_for("login"))
+
+    return f"Perfil de {session['usuario']}"
+
+
+@app.route("/logout")
+def logout():
+
+    session.clear()
+
+    return redirect(url_for("inicio"))
 
 
 if __name__ == "__main__":
